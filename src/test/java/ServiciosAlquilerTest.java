fc035bfe42d5d45a864c0bb19dad8a735c92f02e
Andres Ariza <andres-ariza70@hotmail.com>
2022-10-19 11:44:32 -0500

    Se añadieron pruebas

--- src/test/java/ServiciosAlquilerTest.java
+++ src/test/java/ServiciosAlquilerTest.java
@@ -1,8 +1,11 @@
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.inject.Inject;
 import edu.eci.cvds.samples.entities.Cliente;
+import edu.eci.cvds.samples.entities.Item;
 import edu.eci.cvds.samples.entities.ItemRentado;
+import edu.eci.cvds.samples.entities.TipoItem;
 import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
@@ -15,36 +18,89 @@ public class ServiciosAlquilerTest {
     @Inject
     private SqlSession sqlSession;
     ServiciosAlquiler serviciosAlquiler;
-    int documento;
 
     public ServiciosAlquilerTest() {
         serviciosAlquiler = ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting();
     }
 
-    @Before
-    public void setUp() {
-        documento = 123;
+    @Test
+    public void deberiaInsertarCliente() {
+        Cliente cliente = new Cliente("pupito", 1236969, "1236969", "cra 123", "pupito@gmail.com");
+        try {
+            serviciosAlquiler.registrarCliente(cliente);
+            Assert.assertEquals(serviciosAlquiler.consultarCliente(1236969).getNombre(), "pupito");
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
-    public void emptyDB() {
-        /*
-        for(int i = 0; i < 100; i += 10)
-        { boolean r = false;
-            try {
-                Cliente cliente = serviciosAlquiler.consultarCliente(documento);
-            } catch(ExcepcionServiciosAlquiler e) {
-                r = true;
-            } catch(IndexOutOfBoundsException e) {
-                r = true;
-            }
-
-            // Validate no Client was found;
-            //Assert.assertTrue(r);
-
-        };
-        */
-
-    Assert.assertTrue(true);
+    public void deberiaIConsultarCliente() {
+        try {
+            Assert.assertEquals(serviciosAlquiler.consultarCliente(1236969).getDocumento(), 1236969);
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    @Test
+    public void deberiaVetarCliente() {
+        try {
+            serviciosAlquiler.vetarCliente(1236969, true);
+            Assert.assertTrue(serviciosAlquiler.consultarCliente(1236969).isVetado());
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void noDeberiaConsultarCliente() {
+        try {
+            Assert.assertNull(serviciosAlquiler.consultarCliente(123));
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void deberiaInsertarItem() {
+        TipoItem tipo = new TipoItem(2, "Accion");
+        Item item = new Item(tipo, 639, "Pupito", "Pupito", Date.valueOf("2022-10-02"), 639639, "Pupito", "Pupito");
+        try {
+            serviciosAlquiler.registrarItem(item);
+            Assert.assertEquals(serviciosAlquiler.consultarItem(639).getNombre(), "Pupito");
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void deberiaIConsultarItem() {
+        try {
+            Assert.assertEquals(serviciosAlquiler.consultarItem(639).getId(), 639);
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void deberiaActualizarTarifa() {
+        try {
+            serviciosAlquiler.actualizarTarifaItem(639, 500);
+            Assert.assertEquals(serviciosAlquiler.consultarItem(639).getTarifaxDia(), 500);
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void noDeberiaIConsultarItem() {
+        try {
+            Assert.assertNull(serviciosAlquiler.consultarItem(123));
+        } catch (ExcepcionServiciosAlquiler e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
